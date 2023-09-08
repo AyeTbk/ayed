9cb42dc413ec20735434deb47c7dd80def6c7b6c
Simon Desloges <desloges.simon@gmail.com>
2023-09-08 14:51:27 -0400

    remove ayed-regex, just use the regex crate honestly

--- ayed-regex/src/bin/main.rs
+++
@@ -1,9 +0,0 @@
-use ayed_regex::Regex;
-
-fn main() {
-    let re = Regex::new(r"h(e|a)llo (.*)(!|.)").unwrap();
-
-    dbg!(re.is_match("hello world!"));
-    dbg!(re.is_match("hallo wereld?"));
-    dbg!(re.is_match("bye my guy"));
-}
--- ayed-regex/src/lib.rs
+++
@@ -1,7 +0,0 @@
-pub mod as_iterator;
-pub use as_iterator::AsIterator;
-
-pub mod regex;
-pub use regex::Regex;
-
-mod ast;
