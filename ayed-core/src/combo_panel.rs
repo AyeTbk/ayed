41024ec38a930ad27fe7a18853a7da266d325e94
Simon Desloges <desloges.simon@gmail.com>
2023-11-27 06:53:12 -0500

    make combo panel look pretty

--- ayed-core/src/combo_panel.rs
+++ ayed-core/src/combo_panel.rs
@@ -2,7 +2,7 @@ use crate::{
     grid_string_builder::{Cell, GridStringBuilder},
     input::Input,
     state::State,
-    ui_state::{Color, Span, Style, UiPanel},
+    ui_state::{Span, Style, UiPanel},
     utils::{Position, Size},
 };
 
@@ -18,8 +18,14 @@ impl ComboPanel {
     pub fn render(&mut self, state: &State) -> UiPanel {
         let mut grid = GridStringBuilder::new();
 
+        let header_title = state
+            .active_combo_mode_name
+            .as_ref()
+            .map(String::as_str)
+            .unwrap_or("");
+        let header_text = format!("╌ {header_title}");
         grid.set_cell_span((1, 0), (2, 0));
-        grid.set_cell((1, 0), Cell::new("Combo"));
+        grid.set_cell((1, 0), Cell::new(header_text));
 
         for (i, info) in self.infos.infos.iter().enumerate() {
             let mut serialized_input = String::new();
@@ -33,8 +39,8 @@ impl ComboPanel {
         }
 
         // left and right padding
-        grid.set_cell((0, 0), Cell::new(" "));
-        grid.set_cell((3, 0), Cell::new(" "));
+        grid.set_cell((0, 0), Cell::new("╭─"));
+        grid.set_cell((3, 0), Cell::new("─╮"));
 
         let (size, content) = grid.build();
         let size: Size = size.into();
@@ -42,23 +48,42 @@ impl ComboPanel {
         let line = state.viewport_size.row.saturating_sub(size.row + 1);
         let position = (column, line).into();
 
-        // let position = Position::ZERO.offset((0, 0));
+        let border_style = Style {
+            foreground_color: Some(crate::theme::colors::MODELINE_TEXT),
+            background_color: Some(crate::theme::colors::ACCENT_DARK),
+            ..Default::default()
+        };
+
+        let mut spans = vec![Span {
+            from: Position::ZERO,
+            to: Position::new(size.column, 0),
+            importance: 1,
+            style: border_style,
+        }];
+
+        for row in 1..=size.row {
+            let right_column = size.column.saturating_sub(1);
+            spans.extend([
+                Span {
+                    from: Position::new(0, row),
+                    to: Position::new(0, row),
+                    importance: 1,
+                    style: border_style,
+                },
+                Span {
+                    from: Position::new(right_column, row),
+                    to: Position::new(right_column, row),
+                    importance: 1,
+                    style: border_style,
+                },
+            ]);
+        }
 
         UiPanel {
             position,
             size,
             content,
-            // FIXME This should work but it doesnt. Also, mixing x,y points and sizes with Positions (line,column) is confusing...
-            spans: vec![Span {
-                from: Position::ZERO,
-                to: Position::new(size.column, size.row),
-                importance: 1,
-                style: Style {
-                    background_color: Some(Color::RED),
-                    foreground_color: Some(Color::BLUE),
-                    ..Default::default()
-                },
-            }],
+            spans,
         }
     }
 }
