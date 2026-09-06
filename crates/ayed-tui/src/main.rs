95160632d7c56067426ed472c7f4e5bc19949e7e
Simon Desloges <desloges.simon@gmail.com>
2026-09-06 11:42:51 -0400

    add cmdline option to set workspace dir (working_dir)

--- crates/ayed-tui/src/main.rs
+++ crates/ayed-tui/src/main.rs
@@ -6,9 +6,26 @@ fn main() {
     let mut core = Core::with_builtins();
 
     let mut any_path_specified = false;
-    for arg in std::env::args().skip(1) {
-        core.queue_command(format!("edit {arg}"));
-        any_path_specified = true;
+    let mut args = std::env::args().skip(1);
+    loop {
+        let Some(arg) = args.next() else { break };
+        if let Some(opt) = arg.strip_prefix("--") {
+            match opt {
+                "workspace" => {
+                    let dir_path = args.next().unwrap();
+                    let dir = std::path::absolute(dir_path).unwrap();
+                    assert!(dir.is_dir());
+                    core.state.working_directory = dir;
+                }
+                _ => {
+                    eprintln!("error: unknown option '{opt}'");
+                    std::process::exit(-1);
+                }
+            }
+        } else {
+            core.queue_command(format!("edit {arg}"));
+            any_path_specified = true;
+        }
     }
     if !any_path_specified {
         core.queue_command("edit --scratch".to_string());
